7989431ac09b12d93d604c53f2dc2a54ecfc992c
ErwinPe <erwinp@neuf.fr>
2024-11-27 00:40:03 +0100

    feat: init design code 1

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,11 @@
 <template>
-  <v-app>
-    <v-main class="d-flex flex-column bg-background-100">
-      <router-view :key="$route.path" v-slot="{ Component }" class="h-100">
-        <v-slide-y-transition mode="out-in">
-          <component :is="Component" class="overflow-auto" />
-        </v-slide-y-transition>
-      </router-view>
-    </v-main>
-  </v-app>
+  <div class="h-screen ">
+    <router-view :key="$route.path" v-slot="{ Component }">
+      <component :is="Component" class="overflow-auto" />
+    </router-view>
+  </div>
 </template>
 
 <script setup lang="ts">
 
 </script>
-
-<style scoped>
-main {
-  width: 100%;
-  height: available;
-}
-</style>
--- src/views/DesignCode1.vue
+++ src/views/DesignCode1.vue
@@ -1,3 +1,55 @@
 <template>
-    ok
+    <div class="h-full flex items-center justify-center bg-[#dbe5fb]">
+
+        <div class="bg-white p-[48px] max-w-[787px] max-h-[428px] rounded-xl flex gap-10">
+
+            <img src="../../public//images/design-code-1-product.png" />
+
+            <div class="flex flex-col justify-between">
+
+                <div>
+                    <div class="flex justify-between align-end">
+                        <div class="rounded-full bg-[#020712CC] px-[16px] py-[8px] text-[12px] text-white">
+                            FitLife
+                        </div>
+                        <p class="text-[#02071280] text-[12px]">XV-3855988</p>
+                    </div>
+                    <p class="text-[32px] font-[700] leading-[32px] mt-3">Montre Connectée FitLife Pro 5+</p>
+                    <p class="mt-2">43 notes</p>
+                </div>
+
+
+                <div>
+                    <p class="text-[#02071266] text-[16px] line-through ">183.99 €</p>
+                    <p class="text-[48px] font-[700] leading-[1]">149,99 €</p>
+                </div>
+
+
+                <div class="flex">
+
+                    <button class="bg-[#477AEB] hover:bg-blue-700 text-white py-2 px-4 rounded-xl grow">
+                        Ajouter au pânier
+                    </button>
+                    <button class="bg-[#F6F8FE] text-[#477AEB] rounded-xl">
+                        X
+                    </button>
+
+
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="h-min overflow-hidden rounded-md">
+        <img class="hover:scale-125 transition-all duration-500 cursor-pointer"
+            src="https://images.unsplash.com/photo-1517048676732-d65bc937f952?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            alt="" />
+    </div>
 </template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped></style>
