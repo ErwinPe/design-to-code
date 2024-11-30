d4f5aa42eac14fa93bce5fe8740f88554264028b
ErwinPe <erwinp@neuf.fr>
2024-11-30 04:00:12 +0100

    code cleaning

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,6 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    port: 3000,
-  },
-  define: {
-    "import.meta.env.VITE_BUILD_DATE": JSON.stringify(new Date().toISOString()),
-  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
